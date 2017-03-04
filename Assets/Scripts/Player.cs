f7fffbb953313c0290624aebf6f43d8a89625d09
RogalaJoshua <roga9496@fredonia.edu>
2017-03-04 18:53:47 -0500

    Add files via upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerController))] // Needs the controller script
+public class Player : MonoBehaviour
+{
+
+    public float moveSpeed = 5; // modifies movespeed but since it's public just use the inspector
+    Camera viewCamera; // References the camera in this script
+    PlayerController controller; // Used to reference the controller in this script
+
+    
+    // Use this for initialization
+    void Awake() // For when the player is active
+    {
+        controller = GetComponent<PlayerController>(); // Get the controller
+        viewCamera = Camera.main; // Gets the main camera
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement input
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")); // Gets wasd input to let the player move those directions respectively
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed; // Formula for setting speed
+        controller.Move(moveVelocity); // Calls controller to get movement
+        // Look input
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition); // Get the mouses position from the view of the camera
+        Plane groundPlane = new Plane(Vector3.up, Vector3.up); // Has no real function yet but will make the player view line up with the height of his weapon, just times the second vector 3 by whatever the objects height is
+        float rayDistance; //  will makes sure the ray is within the cameras range
+        if (groundPlane.Raycast(ray, out rayDistance))// Makes sure it's in the right boundries
+        {
+            Vector3 point = ray.GetPoint(rayDistance); // Get the ray distance from the point where the cursor is at
+            //Debug.DrawLine(ray.origin,point,Color.red); //use this if you want to see the line from the camera to the cursor
+            controller.LookAt(point); // Makes the controller look at the cursor point
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]// Any object with physics is going to need this, so I made it mandatory.
+public class PlayerController : MonoBehaviour {
+
+    PlayerController controller;
+    Vector3 velocity; // Gets velocity
+    Rigidbody myRigidbody;
+
+    // Use this for initialization
+    void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody>(); // Used for Getting RigidBody as a variable
+    }
+
+    public void Move(Vector3 _velocity) // Gets movement for player
+    {
+        velocity = _velocity; // Gets the velocity declared at the top and lets it be used in the public method as _velocity
+    }
+
+    public void LookAt(Vector3 lookPoint) // Gets the mouses position and makes the player view it
+    {
+        // Makes it so player does not look down at ground when mouse is in close proximity to player
+        Vector3 HeightCorrectedViewpoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        // Returns point value with heigh correction
+        transform.LookAt(HeightCorrectedViewpoint);
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate () //Using fixedupdate instead of update since it uses a consistent time between calls, is better for update that adjust physics
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime); // Makes the player able to move
+    }
+}
