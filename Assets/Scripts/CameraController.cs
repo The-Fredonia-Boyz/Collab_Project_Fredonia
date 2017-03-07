2eea55cce8e23a8d470bd33672373f72164e50a9
Tim Sherry <timsherry97@gmail.com>
2017-03-07 13:55:13 -0500

    Add files via upload

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    // Reference to the player
+    public GameObject player;
+    
+    // Offset of the camera from the player
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        // Figure out the offset
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    // Used LateUpdate because the dude told me to
+    void LateUpdate()
+    {
+        // Camera is moved to a new position aligned with the player object each frame before displaying what it can see
+        transform.position = player.transform.position + offset;
+    }
+
+
+}
